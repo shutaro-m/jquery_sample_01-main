521c7abc771f30e7fea2c3ad5dd3a95bd90bdcd0
shutaro-m <131327061+shutaro-m@users.noreply.github.com>
2023-06-08 12:27:29 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// buttonSet1
+$( function() {
+
+  $( '#buttonHeader' ).click( function() {
+    $( 'h1' ).text( 'Hello!' );
+  });
+
+  $( '#buttonArticle' ).click( function() {
+    $( 'article' ).css({
+      'color': 'white',
+      'background-color': 'orange',
+      'font-weight':'bold',
+      'font-size':'200%'
+    });
+  });
+
+  $( '#buttonFooter' ).click( function() {
+    $( 'footer' ).html('<p><a href="https://www.google.co.jp">Google</a></p>' );
+    $( 'footer a' ).css( 'color','orange' );
+  });
+
+});
+
+// buttonSet2
+$( function() {
+
+  $( '#buttonNormal' ).click( function() {
+    $('main').css( 'flex-direction','row' );
+  });
+
+  $( '#buttonReverse' ).click( function() {
+    $('main').css( 'flex-direction','row-reverse' );
+  });
+
+  $( '#buttonVertical' ).click( function() {
+    $('main').css( 'flex-direction','column' );
+  });
+
+
+
+});
